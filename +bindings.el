694681a9e5cacec3317c4b3b109ea76e9b07b9ff
Markus Härnvi <markus@harnvi.net>
2020-10-24 11:32:40 +0200

    Initial commit

---
+++ +bindings.el
@@ -0,0 +1,9 @@
+;;; bindings.el -*- lexical-binding: t; -*-
+
+(map!
+ "C-+"   'text-scale-increase
+ "C--"   'text-scale-decrease)
+
+(provide '+bindings)
+
+ ;;; +bindings.el ends here
---
+++ +orgmode.el
@@ -0,0 +1,62 @@
+;;; +orgmode.el -*- lexical-binding: t; -*-
+
+
+(custom-set-faces!
+  '(outline-1 :weight extra-bold :height 1.25)
+  '(outline-2 :weight bold :height 1.15)
+  '(outline-3 :weight bold :height 1.12)
+  '(outline-4 :weight semi-bold :height 1.09)
+  '(outline-5 :weight semi-bold :height 1.06)
+  '(outline-6 :weight semi-bold :height 1.03)
+  '(outline-8 :weight semi-bold)
+  '(outline-9 :weight semi-bold))
+
+
+(after! org
+  (setq org-highlight-latex-and-related '(native script entities))
+
+  ;; make background of fragments transparent
+  (add-hook! 'doom-load-theme-hook
+    (defun +org-refresh-latex-background ()
+      (plist-put! org-format-latex-options
+                  :background
+                  (face-attribute (or (cadr (assq 'default face-remapping-alist))
+                                      'default)
+                                  :background nil t))))
+  (defadvice! org-edit-latex-emv-after-insert ()
+    :after #'org-cdlatex-environment-indent
+    (org-edit-latex-environment))
+  (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
+  (add-hook 'org-mode-hook 'variable-pitch-mode))
+
+
+
+
+(setq org-pandoc-options-for-latex-pdf '((pdf-engine . "xelatex")))
+(setq org-format-latex-header "\\documentclass{article}
+\\usepackage[usenames]{color}
+
+\\usepackage[T1]{fontenc}
+\\usepackage{mathtools}
+\\usepackage{textcomp,amssymb}
+\\usepackage[makeroom]{cancel}
+
+\\usepackage{booktabs}
+
+\\pagestyle{empty}             % do not remove
+% The settings below are copied from fullpage.sty
+\\setlength{\\textwidth}{\\paperwidth}
+\\addtolength{\\textwidth}{-3cm}
+\\setlength{\\oddsidemargin}{1.5cm}
+\\addtolength{\\oddsidemargin}{-2.54cm}
+\\setlength{\\evensidemargin}{\\oddsidemargin}
+\\setlength{\\textheight}{\\paperheight}
+\\addtolength{\\textheight}{-\\headheight}
+\\addtolength{\\textheight}{-\\headsep}
+\\addtolength{\\textheight}{-\\footskip}
+\\addtolength{\\textheight}{-3cm}
+\\setlength{\\topmargin}{1.5cm}
+\\addtolength{\\topmargin}{-2.54cm}
+% my custom stuff
+\\usepackage{arev}
+\\usepackage{arevmath}")
